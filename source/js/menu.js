6e340e4e953786392c874694f5f74a0ee0fe0fad
Yura-by <s.older681@gmail.com>
2019-06-08 19:13:35 +0300

    Шапка готова с js осталась оптимизация фона

---
+++ source/js/menu.js
@@ -0,0 +1,15 @@
+var togglemenu = document.querySelector(".page-header__button");
+var header = document.querySelector(".page-header");
+
+header.classList.remove("page-header--nojs");
+togglemenu.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  header.classList.toggle("page-header--open");
+  if (header.classList.contains("page-header--open")) {
+    togglemenu.classList.remove("page-header__button--open");
+    togglemenu.classList.add("page-header__button--close");
+  } else {
+    togglemenu.classList.remove("page-header__button--close");
+    togglemenu.classList.add("page-header__button--open");
+  }
+})
